62c4adbb22952da998a362a2a43fbb7929a52076
XDZhelheim <601251915@qq.com>
2020-05-05 17:20:11 +0800

    users

--- table_users.sql
+++ table_users.sql
@@ -3,5 +3,8 @@ create table users (
     user_name varchar(255) not null,
     password varchar(20) not null,
     type char not null,
-    unique (user_name, password)
+    unique (user_name, password),
+    check (type in('A', 'P'))
 );
+
+insert into users (user_name, password, type) values ('default_admin', '666666', 'A');
