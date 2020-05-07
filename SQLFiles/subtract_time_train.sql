7a9b509886a4b945ea639ab73cc91850d5d234a3
suarez12138 <60814832+suarez12138@users.noreply.github.com>
2020-05-07 18:17:06 +0800

    Create subtract_time_train.sql

---
+++ SQLFiles/subtract_time_train.sql
@@ -0,0 +1,48 @@
+create function subtract_time_train(leave character varying, arrive character varying, train_n character varying,
+                                    stop1 integer, stop2 integer) returns character varying
+    language plpgsql
+as
+$$
+declare
+    hour1         int;
+    hour2         int;
+    minute1       int;
+    minute2       int;
+    result_hour   int;
+    result_minute int;
+    try1          varchar;
+    try2          varchar;
+begin
+    hour1 = cast(substr(leave, 1, 2) as int);
+    hour2 = cast(substr(arrive, 1, 2) as int);
+    minute1 = cast(substr(leave, 4, 2) as int);
+    minute2 = cast(substr(arrive, 4, 2) as int);
+    result_hour := hour2 - hour1;
+    for i in stop1..stop2 - 1
+        loop
+            select depart_time into try1 from train where train_num = train_n and stop_num = i;
+            if i != stop2 - 1
+            then
+                select depart_time into try2 from train where train_num = train_n and stop_num = i + 1;
+                if cast(substr(try1, 1, 2) as int) > cast(substr(try2, 1, 2) as int)
+                then
+                    result_hour = result_hour + 24;
+                end if;
+            else
+                if cast(substr(try1, 1, 2) as int) > hour2
+                then
+                    result_hour = result_hour + 24;
+                end if;
+            end if;
+        end loop;
+    result_minute := minute2 - minute1;
+    if result_minute < 0 then
+        result_minute := result_minute + 60;
+        result_hour := result_hour - 1;
+    end if;
+    return result_hour || ' 小时 ' || result_minute || ' 分钟';
+end;
+$$;
+
+alter function subtract_time_train(varchar, varchar, varchar, integer, integer) owner to checker;
+
