546d956e35a29d37ca533313e4729732979ac029
suarez12138 <60814832+suarez12138@users.noreply.github.com>
2020-05-07 18:15:38 +0800

    Create modify_price.sql

---
+++ SQLFiles/modify_price.sql
@@ -0,0 +1,37 @@
+create function modify_price(train_n character varying, stop integer, train_type character varying,
+                             arrive character varying) returns double precision
+    language plpgsql
+as
+$$
+declare
+    danjia double precision;
+    leave  varchar;
+    time   varchar;
+    test   double precision;
+begin
+    if stop = 1 then
+        return 0.00;
+    end if;
+    case train_type
+        when '普快' then danjia = 9.95;
+        when '特快' then danjia = 12.05;
+        when '高铁' then danjia = 60.30;
+        when '直达' then danjia = 11.36;
+        when '城际' then danjia = 57.47;
+        when '动车' then danjia = 51.29;
+        when '其它' then danjia = 4.47;
+        end case;
+    select depart_time into leave from train where train_num = train_n and stop_num = 1;
+    time = subtract_time_train(leave, arrive, train_n, 1, stop);
+    test := danjia * (cast((trim(split_part(time, '小', 1))) as double precision)
+        + cast(trim(split_part(split_part(time, '时', 2), '分', 1)) as double precision) / 60.0);
+    if test < 0
+    then
+        test = test + danjia * 24;
+    end if;
+    return round(cast(test as numeric), 2);
+end;
+$$;
+
+alter function modify_price(varchar, integer, varchar, varchar) owner to checker;
+
