86bb4f9f368ee653d383089afa09ba22cd37c1cd
suarez12138 <60814832+suarez12138@users.noreply.github.com>
2020-05-21 17:25:42 +0800

    触发器修改

--- SQLFiles/函数/inquiry_update2.sql
+++ SQLFiles/函数/inquiry_update2.sql
@@ -17,42 +17,42 @@ begin
     from train
     where train.train_id = new.train_id;
     if new.stop_num = 1 then
-        return 0.00;
+        test = 0.00;
+    else
+        case type
+            when '普快' then danjia = 9.95;
+            when '特快' then danjia = 12.05;
+            when '高铁' then danjia = 60.30;
+            when '直达' then danjia = 11.36;
+            when '城际' then danjia = 57.47;
+            when '动车' then danjia = 51.29;
+            when '其它' then danjia = 4.47;
+            end case;
+
+        select i.depart_time into leave from schedule i where i.train_id = new.train_id and i.stop_num = 1;
+        time = subtract_time_train(leave, new.arrive_time, train_n, 1, new.stop_num);
+
+
+        test := danjia * (cast((trim(split_part(time, '小', 1))) as double precision)
+            + cast(trim(split_part(split_part(time, '时', 2), '分', 1)) as double precision) / 60.0);
+        select price_from_start_station
+        into lasttest
+        from inquire_table s
+        where s.train_num = train_n
+          and stop_num = new.stop_num - 1;
+        if test < 0 or test < lasttest
+        then
+            test = test + danjia * 24;
+        end if;
     end if;
-    case type
-        when '普快' then danjia = 9.95;
-        when '特快' then danjia = 12.05;
-        when '高铁' then danjia = 60.30;
-        when '直达' then danjia = 11.36;
-        when '城际' then danjia = 57.47;
-        when '动车' then danjia = 51.29;
-        when '其它' then danjia = 4.47;
-        end case;
-
-    select i.depart_time into leave from schedule i where i.train_id = new.train_id and i.stop_num = 1;
-    time = subtract_time_train(leave, new.arrive_time, train_n, 1, new.stop_num);
-
-
-    test := danjia * (cast((trim(split_part(time, '小', 1))) as double precision)
-        + cast(trim(split_part(split_part(time, '时', 2), '分', 1)) as double precision) / 60.0);
-    select price_from_start_station
-    into lasttest
-    from inquire_table s
-    where s.train_num = train_n
-      and stop_num = new.stop_num - 1;
-    if test < 0 or test < lasttest
-    then
-        test = test + danjia * 24;
-    end if;
-
     update inquire_table i
     set train_num=train_n,
         stop_num=new.stop_num,
         station_name=s.station_name,
         arrive_time=new.arrive_time,
         depart_time=new.depart_time,
-        train_type=round(cast(test as numeric), 2),
-        price_from_start_station=test
+        train_type=type,
+        price_from_start_station=round(cast(test as numeric), 2)
     from station s
     where i.schedule_id = new.schedule_id
       and s.station_id = new.station_id;
